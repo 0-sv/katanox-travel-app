0aa4b94900ce5e099098c03af7941098834ebac1
Sebastiaan Verbeek <sebastiaan.verbeek@redkubes.com>
2021-10-27 09:11:10 +0200

    feat: add static linting

---
+++ src/components/Table.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface Props {
+  hotelId: number
+  name: string
+  address: string
+  starRating: number
+}
+
+export default ({}: Props): React.ReactElement => {
+  return <div></div>
+}
