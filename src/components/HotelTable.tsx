9319c21469098acf0dc4fbee908e37b1283dae33
Sebastiaan Verbeek <sebastiaan.verbeek@redkubes.com>
2021-10-29 20:11:49 +0200

    feat: task 1 part 2 - delete row

--- src/components/HotelTable.tsx
+++ src/components/HotelTable.tsx
@@ -1,44 +1,52 @@
 import React, { useState } from 'react'
 import Table from 'rc-table'
-
-const deleteRow = (row: React.ReactNode) => {
-  console.info(row)
-}
+import { DefaultRecordType } from 'rc-table/lib/interface'
 
 // TD: column metadata can also come from a decent ORM,
 // so we shouldn't have to initialize it manually here.
-const columns = [
-  {
-    title: 'Hotel ID',
-    dataIndex: 'id',
-    key: 'id',
-    width: 150,
-  },
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    width: 150,
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-    width: 250,
-  },
-  {
-    title: 'Delete',
-    dataIndex: 'delete',
-    key: 'delete',
-    render: (row: React.ReactNode) => (
-      <button onClick={() => deleteRow(row)}></button>
-    ),
-  },
-]
-
 const HotelTable = (): React.ReactElement => {
-  const [data] = useState(JSON.parse(localStorage.getItem('hotels') || '{}'))
-  return <Table columns={columns} data={data} />
+  const [hotels, setHotels] = useState(
+    JSON.parse(localStorage.getItem('hotels') || '{}')
+  )
+  return (
+    <Table
+      columns={[
+        {
+          title: 'Hotel ID',
+          dataIndex: 'id',
+          key: 'id',
+          width: 150,
+        },
+        {
+          title: 'Name',
+          dataIndex: 'name',
+          key: 'name',
+          width: 150,
+        },
+        {
+          title: 'Address',
+          dataIndex: 'address',
+          key: 'address',
+          width: 250,
+        },
+        {
+          title: 'Delete',
+          dataIndex: '',
+          key: 'd',
+          render: (record: DefaultRecordType) => (
+            <button
+              onClick={() =>
+                setHotels(
+                  hotels.filter((h: DefaultRecordType) => h.id !== record.id)
+                )
+              }
+            />
+          ),
+        },
+      ]}
+      data={hotels}
+    />
+  )
 }
 
 export default HotelTable
