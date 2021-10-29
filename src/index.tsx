1409aa002af0888307a5329a7fb6e0afe8583b17
Sebastiaan Verbeek <sebastiaan.verbeek@redkubes.com>
2021-10-29 08:11:19 +0200

    feat: add water.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import { Helmet } from 'react-helmet'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+      <link
+        rel='stylesheet'
+        href='https://cdn.jsdelivr.net/npm/water.css@2/out/water.css'
+      />
+    </Helmet>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
