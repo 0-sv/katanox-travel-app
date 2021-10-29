824a2c4842b78f02c16cb484d081910ed8359661
Sebastiaan Verbeek <sebastiaan.verbeek@redkubes.com>
2021-10-29 09:47:11 +0200

    fix: type error on render again

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { Helmet } from 'react-helmet'
-import { nanoid } from 'nanoid'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
@@ -19,32 +18,4 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
-localStorage.setItem(
-  'hotels',
-  JSON.stringify([
-    {
-      name: 'Hilton Rotterdam',
-      address: 'Weena 10 3012 CM Rotterdam',
-      id: nanoid(5),
-    },
-    {
-      name: 'Hilton Garden Inn Leiden',
-      address: 'Willem Einthovenstraat 3 Oegstgeest',
-      id: nanoid(5),
-    },
-    {
-      name: 'Four Seasons Resort Dubai',
-      address: '2 Jumeirah St',
-      id: nanoid(5),
-    },
-    {
-      name: 'Four Seasons Hotel Prague',
-      address: 'Veleslavinova 1098 2a Prague',
-      id: nanoid(5),
-    },
-  ])
-)
