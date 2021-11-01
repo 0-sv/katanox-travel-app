e6e0133fc449e61e78a10e5cb65a4ab6e12bd035
Sebastiaan Verbeek <sebastiaan.verbeek@redkubes.com>
2021-11-01 08:37:35 +0100

    feat: add autocomplete template

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './App.css'
+import AutoComplete from './components/Autoocomplete'
 import HotelTable from './components/HotelTable'
 
 // TD: we didn't include any other components for a usual web app,
 // like a nice <Nav/> or </Footer>.
-const App = (): React.ReactElement => <HotelTable />
+const App = (): React.ReactElement => (
+  <div className='App'>
+    <AutoComplete />
+    <HotelTable />
+  </div>
+)
 
 export default App
---
+++ src/components/Autoocomplete.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const AutoComplete = (): React.ReactElement => <input />
+
+export default AutoComplete
