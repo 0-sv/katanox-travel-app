3776acd18132fe2e8f7fae834f046892859b67e0
Sebastiaan Verbeek <sebastiaan.verbeek@redkubes.com>
2021-11-03 06:53:13 +0100

    fix: make all cells clickable

--- src/components/HotelTable.tsx
+++ src/components/HotelTable.tsx
@@ -14,76 +14,82 @@ const HotelTable = ({
   setHotels,
   selected,
   setSelected,
-}: Props): React.ReactElement => (
+}: Props): React.ReactElement => {
+  const handleCellClick = (record: DefaultRecordType) => ({
+    onClick() {
+      // TD: very hacky way to check that we've filtered down to one hotel,
+      // in hindsight would've used Redux.
+      if (
+        hotels.map((hotel) => hotel.nameFilter).filter(Boolean).length <
+        hotels.length
+      )
+        console.log(`${JSON.stringify(record)}`)
+    },
+  })
+
   // TD: I was looking for an easy Table component...
   // rc-table has quite inconsistent behaviour, but is easy to use
   // Would definitely pick another Table component library for the next project.
-  <Table
-    // TD: column metadata can also come from a decent ORM,
-    // so we shouldn't have to initialize it manually here.
-    columns={[
-      {
-        title: 'Hotel ID',
-        dataIndex: 'id',
-        key: 'id',
-        width: 150,
-      },
-      {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        width: 150,
-        // TD: extract this into a function to generalize for all columns
-        onCell: (record) => ({
-          onClick() {
-            // TD: very hacky way to check that we've filtered down to one hotel,
-            // in hindsight would've used Redux.
-            if (
-              hotels.map((hotel) => hotel.nameFilter).filter(Boolean).length ===
-              1
-            )
-              console.log(`${record.name}`)
-          },
-        }),
-      },
-      {
-        title: 'Address',
-        dataIndex: 'address',
-        key: 'address',
-        width: 250,
-      },
-      {
-        title: 'Delete',
-        dataIndex: '',
-        key: 'delete',
-        render: (record: DefaultRecordType) => (
-          <button
-            onClick={() =>
-              setHotels(hotels.filter((hotel) => hotel.id !== record.id))
-            }
-          />
-        ),
-      },
-      {
-        title: 'Select',
-        dataIndex: '',
-        key: 'select',
-        render: (value: any, record: DefaultRecordType, index: number) => (
-          <input
-            type='checkbox'
-            onClick={(e) => {
-              setSelected(
-                Object.assign(selected, {
-                  [index]: (e.target as HTMLInputElement).checked,
-                })
-              )
-            }}
-          />
-        ),
-      },
-    ]}
-    data={hotels.filter((hotel) => hotel.nameFilter)}
-  />
-)
+  return (
+    <Table
+      // TD: column metadata can also come from a decent ORM,
+      // so we shouldn't have to initialize it manually here.
+      columns={[
+        {
+          title: 'Hotel ID',
+          dataIndex: 'id',
+          key: 'id',
+          width: 150,
+          onCell: handleCellClick,
+        },
+        {
+          title: 'Name',
+          dataIndex: 'name',
+          key: 'name',
+          width: 150,
+          // TD: extract this into a function to generalize for all columns
+          onCell: handleCellClick,
+        },
+        {
+          title: 'Address',
+          dataIndex: 'address',
+          key: 'address',
+          width: 250,
+          onCell: handleCellClick,
+        },
+        {
+          title: 'Delete',
+          dataIndex: '',
+          key: 'delete',
+          render: (record: DefaultRecordType) => (
+            <button
+              onClick={() =>
+                setHotels(hotels.filter((hotel) => hotel.id !== record.id))
+              }
+            />
+          ),
+        },
+        {
+          title: 'Select',
+          dataIndex: '',
+          key: 'select',
+          render: (value: any, record: DefaultRecordType, index: number) => (
+            <input
+              type='checkbox'
+              onClick={(e) => {
+                setSelected(
+                  Object.assign(selected, {
+                    [index]: (e.target as HTMLInputElement).checked,
+                  })
+                )
+              }}
+            />
+          ),
+        },
+      ]}
+      data={hotels.filter((hotel) => hotel.nameFilter)}
+    />
+  )
+}
 
 export default HotelTable
